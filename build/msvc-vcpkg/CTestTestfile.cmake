9085e53c687030afb9435a84dc6699a12b43f51a
Göktug Akbulutlar <goktug_akbulutlar@trimble.com>
2026-04-15 20:04:28 +0200

    Remove build folder from tracking but keep local files

--- build/msvc-vcpkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gakbulu/FilterVoxelgrid/Downsampling-a-PointCloud-using-a-VoxelGrid-filter
-# Build directory: C:/Users/Gakbulu/FilterVoxelgrid/Downsampling-a-PointCloud-using-a-VoxelGrid-filter/build/msvc-vcpkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
